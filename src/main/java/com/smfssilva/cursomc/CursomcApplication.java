f38f00b341c46b174c0743155d41593e8fd02da8
smfssilva <smfssilva@yahoo.com>
2021-08-06 17:05:57 -0300

    Estado e cidade

--- src/main/java/com/smfssilva/cursomc/CursomcApplication.java
+++ src/main/java/com/smfssilva/cursomc/CursomcApplication.java
@@ -3,8 +3,12 @@ package com.smfssilva.cursomc;
 import java.util.Arrays;
 
 import com.smfssilva.cursomc.domain.Categoria;
+import com.smfssilva.cursomc.domain.Cidade;
+import com.smfssilva.cursomc.domain.Estado;
 import com.smfssilva.cursomc.domain.Produto;
 import com.smfssilva.cursomc.repositories.CategoriaRepository;
+import com.smfssilva.cursomc.repositories.CidadeRepository;
+import com.smfssilva.cursomc.repositories.EstadoRepository;
 import com.smfssilva.cursomc.repositories.ProdutoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +24,12 @@ public class CursomcApplication implements CommandLineRunner{
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -44,6 +54,22 @@ public class CursomcApplication implements CommandLineRunner{
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+
+
 	}
 
 }
