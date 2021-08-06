103397038b975d48a478f3ed32a4a151fd33ae3d
smfssilva <smfssilva@yahoo.com>
2021-08-06 13:26:59 -0300

    Crianndo operação de instanciação

--- src/main/java/com/smfssilva/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/smfssilva/cursomc/resources/CategoriaResource.java
@@ -1,7 +1,7 @@
 package com.smfssilva.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.smfssilva.cursomc.domain.Categoria;
+import com.smfssilva.cursomc.services.CategoriaService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,9 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.smfssilva.cursomc.domain.Categoria;
-import com.smfssilva.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
