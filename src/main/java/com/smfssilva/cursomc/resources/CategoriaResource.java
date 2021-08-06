c6c4468a814f153bf439165163a60693cece346b
smfssilva <smfssilva@yahoo.com>
2021-08-06 11:52:58 -0300

    Banco de dados H2 e criação automatica dos objetos

--- src/main/java/com/smfssilva/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/smfssilva/cursomc/resources/CategoriaResource.java
@@ -3,11 +3,11 @@ package com.smfssilva.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.smfssilva.cursomc.domain.Categoria;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.smfssilva.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
