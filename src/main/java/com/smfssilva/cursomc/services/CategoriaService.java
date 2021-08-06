c22467196774ff86cc8e45137b5fb2ca2d45ab9e
smfssilva <smfssilva@yahoo.com>
2021-08-06 15:47:08 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/smfssilva/cursomc/services/CategoriaService.java
+++ src/main/java/com/smfssilva/cursomc/services/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import com.smfssilva.cursomc.domain.Categoria;
 import com.smfssilva.cursomc.repositories.CategoriaRepository;
+import com.smfssilva.cursomc.services.exceptions.ObjectNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,7 @@ public class CategoriaService {
     public Categoria buscar(Integer id){
         
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+            "Objeto não encontrado! Id:" + id + ", Tipo: " + Categoria.class.getName()));
     }
 }
